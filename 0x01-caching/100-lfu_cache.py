e23c25ece5ff9a02d13e1b20f4a609aebda1ff22
Dawitdkt <dawitkinfu5@gmail.com>
2023-02-25 03:31:33 +0300

    some fix

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -5,45 +5,50 @@ from collections import OrderedDict
 
 
 class LFUCache(BaseCaching):
-    """ Class that inherits from BaseCaching and is a caching system """
+    """LFU caching system"""
+
     def __init__(self):
+        """Initialize the cache"""
         super().__init__()
-        self.lru_cache = OrderedDict()
-        self.lfu_cache = {}
+        self.lfu = {}  # store the frequency of each key
+        self.lru = {}  # store the recency of each key
 
     def put(self, key, item):
-        """ Assign to the dictionary, LFU algorithm """
-        if key in self.lru_cache:
-            del self.lru_cache[key]
-        if len(self.lru_cache) > BaseCaching.MAX_ITEMS - 1:
-            min_value = min(self.lfu_cache.values())
-            lfu_keys = [k for k, v in self.lfu_cache.items() if v == min_value]
-            if len(lfu_keys) == 1:
-                print("DISCARD:", lfu_keys[0])
-                self.lru_cache.pop(lfu_keys[0])
-                del self.lfu_cache[lfu_keys[0]]
-            else:
-                for k, _ in list(self.lru_cache.items()):
-                    if k in lfu_keys:
-                        print("DISCARD:", k)
-                        self.lru_cache.pop(k)
-                        del self.lfu_cache[k]
-                        break
-        self.lru_cache[key] = item
-        self.lru_cache.move_to_end(key)
-        if key in self.lfu_cache:
-            self.lfu_cache[key] += 1
+        """Assign the item to the key in the cache"""
+        if key is None or item is None:
+            return
+        if key in self.cache_data:
+            self.cache_data[key] = item  # update the value
+            self.lfu[key] += 1  # increase the frequency
+            self.lru[key] = len(self.lru)  # update the recency
         else:
-            self.lfu_cache[key] = 1
-        self.cache_data = dict(self.lru_cache)
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                # find the least frequency used key
+                min_freq = min(self.lfu.values())
+                lfu_keys = [k for k, v in self.lfu.items() if v == min_freq]
+                if len(lfu_keys) == 1:
+                    # only one key with minimum frequency
+                    lfu_key = lfu_keys[0]
+                else:
+                    # more than one key with minimum frequency
+                    # use LRU algorithm to break ties
+                    min_rec = min(self.lru[k] for k in lfu_keys)
+                    lfu_key = [
+                        k for k in lfu_keys if self.lru[k] == min_rec][0]
+                print("DISCARD: {}".format(lfu_key))
+                del self.cache_data[lfu_key]  # discard the key
+                del self.lfu[lfu_key]  # remove from frequency dict
+                del self.lru[lfu_key]  # remove from recency dict
+
+            self.cache_data[key] = item  # add the new key and value
+            self.lfu[key] = 1  # set the initial frequency to 1
+            self.lru[key] = len(self.lru)  # set the initial recency to max
 
     def get(self, key):
-        """ Return the value linked """
-        if key in self.lru_cache:
-            value = self.lru_cache[key]
-            self.lru_cache.move_to_end(key)
-            if key in self.lfu_cache:
-                self.lfu_cache[key] += 1
-            else:
-                self.lfu_cache[key] = 1
-            return value
+        """Return the value associated with the key in the cache"""
+        if key is None or key not in self.cache_data:
+            return None
+        value = self.cache_data[key]
+        self.lfu[key] += 1  # increase the frequency of access
+        self.lru[key] = len(self.lru)  # update the recency of access
+        return value
